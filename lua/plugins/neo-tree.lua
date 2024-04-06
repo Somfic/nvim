45632604b2afad6ef9e6b7bf73abd8a598b3c65e
Lucas <29230467+Somfic@users.noreply.github.com>
2024-04-06 15:54:54 +0200

    Open file explorer with ctrl p

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,6 @@ return {
 	'MunifTanjim/nui.nvim'
   },
   config = function ()
-	vim.keymap.set('n', '<C-t>', ':Neotree filesystem reveal right<CR>')
+	vim.keymap.set('n', '<C-p>', ':Neotree filesystem reveal right<CR>')
   end
 }
