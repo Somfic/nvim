07ecc3e409457b54d1549ca26cf4cca12957c32d
Lucas <29230467+Somfic@users.noreply.github.com>
2024-04-07 02:53:40 +0200

    Added lazygit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,10 +1,29 @@
 return {
-	{
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		require('gitsigns').setup()
-	end
-}, {
-	"tpope/vim-fugitive",
-}
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup()
+    end
+  }, {
+  "tpope/vim-fugitive",
+},
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "git", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  }
 }
