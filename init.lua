b799442caccdcea804efe4335b53a1bb725bf0c3
Lucas <lucas@somfic.dev>
2025-05-02 01:03:10 +0200

    🔧 fixes key mappings and cloning logic in setup functions
    semver: patch

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	endvim.fn.system({
+	vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -3,16 +3,21 @@ return {
 		"stevearc/oil.nvim",
 		dependencies = "nvim-tree/nvim-web-devicons",
 		lazy = false,
-		keys = {
-			{ "<leader>vf", ":Oil --float<CR>", desc = "files" },
-		},
 		config = function()
-			require("oil").setup({
+			local oil = require("oil")
+
+			oil.setup({
 				default_file_explorer = true,
 				columns = {
 					"icon",
 				},
+				keymaps = {
+					["-"] = false, -- Disable default keymap for '-'
+				},
 			})
+
+			vim.keymap.set("n", "<BS>", oil.open, { desc = "Go to parent directory" })
+			vim.keymap.set("n", "-", oil.toggle_float	, { desc = "Close floating window" })
 		end,
 	},
 }
