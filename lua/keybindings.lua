4233263327b76f2ad98f5e607a0948f5d4fe0ffb
Lucas <lucas@somfic.dev>
2025-10-10 09:32:56 +0200

    ✨ add WASD navigation keybindings in `keybindings.lua`
    semver: minor

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -7,6 +7,18 @@ require('which-key').setup({
 	preset = 'helix',
 })
 
+-- WASD navigation
+vim.keymap.set({ 'n', 'v' }, 'w', 'k', { desc = 'Move up' })
+vim.keymap.set({ 'n', 'v' }, 's', 'j', { desc = 'Move down' })
+vim.keymap.set({ 'n', 'v' }, 'a', 'b', { desc = 'Word backward' })
+vim.keymap.set({ 'n', 'v' }, 'd', 'w', { desc = 'Word forward' })
+vim.keymap.set({ 'n', 'v' }, 'A', 'h', { desc = 'Character left' })
+vim.keymap.set({ 'n', 'v' }, 'D', 'l', { desc = 'Character right' })
+vim.keymap.set({ 'n', 'v' }, 'W', '{', { desc = 'Previous code block' })
+vim.keymap.set({ 'n', 'v' }, 'S', '}', { desc = 'Next code block' })
+vim.keymap.set({ 'n', 'v' }, '<M-a>', '^', { desc = 'Line start (non-blank)' })
+vim.keymap.set({ 'n', 'v' }, '<M-d>', '$', { desc = 'Line end' })
+
 -- Global completion trigger
 vim.keymap.set('i', '<C-Space>', function()
 	-- Try to trigger nvim-cmp completion first
