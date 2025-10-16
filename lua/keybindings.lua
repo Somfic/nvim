3d1387ea3e462b184bc58b811646679a0af8dae3
Lucas <lucas@somfic.dev>
2025-10-17 01:07:45 +0200

    🚸 enhance keybindings for navigation and clipboard actions
    semver: patch

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -7,21 +7,35 @@ require('which-key').setup({
 	preset = 'helix',
 })
 
--- w and s to move a line up and down
+-- line navigation
 vim.keymap.set({ 'n', 'v' }, 'w', 'k', { desc = 'Line up' })
 vim.keymap.set({ 'n', 'v' }, 's', 'j', { desc = 'Line down' })
+vim.keymap.set({ 'n', 'v' }, 'A', '^', { desc = 'Beginning of line' })
+vim.keymap.set({ 'n', 'v' }, 'D', '$', { desc = 'End of line' })
 
--- shift w and s to jump between code blocks
+-- block navigation
 vim.keymap.set({ 'n', 'v' }, 'W', '{', { desc = 'Jump to previous block' })
 vim.keymap.set({ 'n', 'v' }, 'S', '}', { desc = 'Jump to next block' })
 
--- a and d to move left and right
+-- character navigation
 vim.keymap.set({ 'n', 'v' }, 'a', 'h', { desc = 'Move left' })
 vim.keymap.set({ 'n', 'v' }, 'd', 'l', { desc = 'Move right' })
 
--- shift a and d to move a word left and right
-vim.keymap.set({ 'n', 'v' }, 'A', 'b', { desc = 'Word backward' })
-vim.keymap.set({ 'n', 'v' }, 'D', 'w', { desc = 'Word forward' })
+-- word navigation
+vim.keymap.set({ 'n', 'v' }, 'q', 'b', { desc = 'Word backward' })
+vim.keymap.set({ 'n', 'v' }, 'e', 'w', { desc = 'Word forward' })
+
+-- clipboard
+vim.keymap.set({ 'n', 'v' }, 'x', 'dd', { desc = 'Cut' })
+vim.keymap.set({ 'n', 'v' }, 'p', 'p', { desc = 'Paste' })
+
+-- moving lines
+vim.keymap.set('i', '<M-w>', '<Esc>:m .-2<CR>==gi', { desc = 'Move line up', noremap = true, silent = true })
+vim.keymap.set('n', '<M-w>', ':m .-2<CR>==', { desc = 'Move line up', noremap = true, silent = true })
+vim.keymap.set('n', '<M-s>', ':m .+1<CR>==', { desc = 'Move line down', noremap = true, silent = true })
+vim.keymap.set('i', '<M-s>', '<Esc>:m .+1<CR>==gi', { desc = 'Move line down', noremap = true, silent = true })
+vim.keymap.set('v', '<M-w>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up', noremap = true, silent = true })
+vim.keymap.set('v', '<M-s>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down', noremap = true, silent = true })
 
 -- Global completion trigger
 vim.keymap.set('i', '<C-Space>', function()
