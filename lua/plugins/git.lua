9790c5978de3cc1fec42b1752666a63a5355a819
Lucas <lucas@somfic.dev>
2026-04-12 02:27:33 +0200

    use lazygit in nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -17,57 +17,32 @@ return {
                     delay = 300,
                 },
             })
-        end,
-    },
 
-    {
-        -- Git commands helper
-        "NeogitOrg/neogit",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "sindrets/diffview.nvim",
-            "nvim-telescope/telescope.nvim",
-        },
-        config = function()
-            local neogit = require("neogit")
-
-            neogit.setup({
-                integrations = {
-                    telescope = true,
-                    diffview = true,
-                },
-            })
-
-            -- Keymaps
-            vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Neogit status" })
-            vim.keymap.set("n", "<leader>gc", "<cmd>Neogit commit<cr>", { desc = "Git commit" })
-            vim.keymap.set("n", "<leader>gP", "<cmd>Neogit push<cr>", { desc = "Git push" })
-            vim.keymap.set("n", "<leader>gp", "<cmd>Neogit pull<cr>", { desc = "Git pull" })
             vim.keymap.set("n", "<leader>gb", function()
                 require("gitsigns").blame_line({ full = true })
             end, { desc = "Git blame line" })
             vim.keymap.set("n", "<leader>gB", function()
                 require("gitsigns").toggle_current_line_blame()
             end, { desc = "Toggle inline blame" })
-
-            -- Simple commit workflow (with prompt)
-            vim.keymap.set("n", "<leader>gC", function()
-                local msg = vim.fn.input("Commit message: ")
-                if msg ~= "" then
-                    vim.cmd(string.format('!git add . && git commit -m "%s"', msg))
-                    vim.notify("Committed all changes", vim.log.levels.INFO)
-                end
-            end, { desc = "Stage all & commit" })
-
-            -- Quick commit and push workflow
-            vim.keymap.set("n", "<leader>gQ", function()
-                local msg = vim.fn.input("Commit message: ")
-                if msg ~= "" then
-                    vim.cmd(string.format('!git add . && git commit -m "%s" && git push', msg))
-                    vim.notify("Committed and pushed!", vim.log.levels.INFO)
-                end
-            end, { desc = "Quick commit & push" })
         end,
+    },
+
+    {
+        -- Lazygit in a floating window — handles commit/push/pull/stash/etc.
+        "kdheepak/lazygit.nvim",
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        keys = {
+            { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+        },
     },
 
     {
